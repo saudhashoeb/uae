fe08bcdfaa25979c868601c8eebf7edab38fed18
saudhashoeb <saudhashoeb02@gmail.com>
2020-06-17 12:21:09 -0400

    my project

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('.swiper_container', {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper_pagination',
+        clickable: true,
+      },
+    });
+
+    function openCity(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
+      
+      // Get the element with id="defaultOpen" and click on it
+      document.getElementById("defaultOpen").click();
+
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
---
+++ task/README.md
@@ -0,0 +1,39 @@
+# Web
+Resources used for Web Application development
+
+- [SASS](http://sass-lang.com/) - Preprocessing. *sass --watch css:css*
+
+> SourceCode path " /... "
+
+*Plugin used*
+
+> 1] *- [Bootstrap 4.1](https://getbootstrap.com/)*
+
+> 2] *- [fontawesome-free-4.7](https://fontawesome.com/v4.7.0/)*
+
+> 3] *- [Swiper-4.5](https://idangero.us/swiper/)*
+
+> 4] *- [animate.min](https://daneden.github.io/animate.css/)*
+
+> 5] *- [OwlCarousel2](https://owlcarousel2.github.io/OwlCarousel2/)*
+
+> 6] *- [Counter-Up](https://github.com/bfintal/Counter-Up)*
+
+> 7] *- [Wow](https://github.com/matthieua/WOW)*
+
+
+
+
+
+
+
+
+
+
+
+-------
+
+
+-------
+
+-------
